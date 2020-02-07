08a1eea9b16d41bf16a30e769176097617405ba5
eva-yanrong-chen <eva.yanrong.chen@gmail.com>
2020-02-07 13:40:19 -0500

    Add route /todos/create; Syschronous data request using html form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,6 +15,16 @@ class Todo(db.Model):
         return f'<Todo: {self.id} {self.description}>'
 
 db.create_all()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '') # the '' is a default empty string in case no text came in
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    #   After adding new todo item to the table, the app will redirect to '/' route
+    return redirect(url_for('index'))
 
 
 @app.route('/')
